5e855cd8ed402ec51fd6710efe1115d461db510a
Sander Vanhove <sandervhove@gmail.com>
2024-10-23 22:34:40 +0200

    Add UI graveyard

--- src/addons/godot-russion-roulette/roulette.gd
+++ src/addons/godot-russion-roulette/roulette.gd
@@ -1,41 +1,105 @@
-extends Node2D
+@tool
+extends Node
 
 
 const ExplosionScene: PackedScene = preload("res://addons/godot-russion-roulette/explosion.tscn")
 
+@onready var _node_graveyard: Container = %NodeGraveyard
+@onready var _trigger_audio: AudioStreamPlayer = $Audio/TriggerAudio
+@onready var _shot_audio: AudioStreamPlayer = $Audio/ShotAudio
 
-@onready var _trigger_audio: AudioStreamPlayer = $TriggerAudio
-@onready var _shot_audio: AudioStreamPlayer = $ShotAudio
+var editor_base_control: Control
+
+var _focussed_node: Node
+var _shake_duration = 0.0
+var _shake_intensity = 0.0
+var _exploded_nodes: Dictionary = {}
+
+
+var _chance: float = 100.0
+var _sound_enabled: bool = true
+var _screen_shake_enabled: bool = true
 
 
 func _ready() -> void:
-	print("init")
-	#get_viewport().gui_focus_changed.connect(_on_focus_changed)
+	set_process(false)
 
 
-#func _input(event: InputEvent) -> void:
-	#if not event is InputEventMouseButton or not event.pressed or event.button_index != MOUSE_BUTTON_LEFT:
-		#return
-#
-	##_trigger_audio.global_position = get_global_mouse_position()
-	#_trigger_audio.play()
-	#var explosion: Node2D = ExplosionScene.instantiate()
-	#explosion.position = get_global_mouse_position()
-	#get_tree().root.add_child.call_deferred(explosion)
-#
-#
-#func _on_focus_changed(node: Control):
-	#
-	#if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		#return
-	#if randf() < 0.9999:
-		#return
-#
-	##_shot_audio.global_position = get_global_mouse_position()
-	#_shot_audio.play()
-	#node.queue_free()
+func _exit_tree() -> void:
+	reset()
+
+
+func _process(delta):
+	if _shake_duration > 0:
+		_shake_duration -= delta
+		editor_base_control.position = Vector2(
+			randf_range(-_shake_intensity, _shake_intensity), 
+			randf_range(-_shake_intensity, _shake_intensity)
+		)
+	else:
+		set_process(false)
+		editor_base_control.position = Vector2.ZERO
+
+
+func _input(event: InputEvent) -> void:
+	if not event is InputEventMouseButton or not event.pressed or event.button_index != MOUSE_BUTTON_LEFT:
+		return
+
+	await get_tree().process_frame
+	if randf() > 1.0 / _chance:
+		if _sound_enabled:
+			_trigger_audio.play()
+		return
+
+	var node: Control = get_window().gui_get_focus_owner()
+	if node and not node.find_parent("Roulette"):
+		if _sound_enabled:
+			_shot_audio.play()
+			
+		_exploded_nodes[node] = node.get_parent()
+		node.custom_minimum_size = node.size
+		node.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
+		node.size_flags_vertical = Control.SIZE_SHRINK_BEGIN
+		node.reparent(_node_graveyard, false)
+		
+		shake_screen(0.1, 5.0)
+		spawn_explosion()
+
+
+func shake_screen(duration, intensity):
+	if not _screen_shake_enabled:
+		return
+
+	_shake_duration = duration
+	_shake_intensity = intensity
+	set_process(true)
+
+
+func spawn_explosion():
+	var explosion = ExplosionScene.instantiate()
+	get_viewport().add_child(explosion, true)
+	explosion.position = explosion.get_global_mouse_position()
+	explosion.play()
+	
+	
+func reset():
+	for node in _exploded_nodes:
+		if not is_instance_valid(node) or not is_instance_valid(_exploded_nodes[node]):
+			continue
+		node.reparent(_exploded_nodes[node], false)
+
+
+func _on_chance_spin_box_value_changed(value: float) -> void:
+	_chance = value
+
+
+func _on_sound_check_button_toggled(toggled_on: bool) -> void:
+	_sound_enabled = toggled_on
+
+
+func _on_screen_shake_check_button_toggled(toggled_on: bool) -> void:
+	_screen_shake_enabled = toggled_on
 
 
 func _on_button_pressed() -> void:
-	print("A thing")
-	_shot_audio.play()
+	reset()
